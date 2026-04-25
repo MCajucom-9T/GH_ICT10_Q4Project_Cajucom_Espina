880c684b2cc299d53846be16dff31a61fe15f223
KEspina-10T <espina.ka@obmontessori.edu.ph>
2026-04-25 23:07:08 +0800

    Update main.py

--- attendance/main.py
+++ attendance/main.py
@@ -1,25 +1,17 @@
-# Numerical Python and MATLAB Plotting Library
-from pyscript import display, document
-import numpy as np # np is just a common alias
+from pyscript import display, document #type: ignore (quick fix feature)
+import numpy as np
 import logging
 logging.getLogger('matplotlib').setLevel(logging.ERROR)
 import matplotlib.pyplot as plt
 
+def graph(e):
+    document.getElementById('output').innerHTML=''
+    Month = np.array([document.getElementById('input2').value])
+    Absentees = np.array([int(document.getElementById('input1').value)])
 
-plt.figure()
-plt.plot([0, 1], [0, 1])
-plt.close()
+    Academic_Attendance_Sheet =plt.plot(Month,Absentees , marker='D')
 
-def smallgraph(e):
-
-
-    absences = np.array([int(document.getElementById("input").value)])
-    month = np.array([(document.getElementById("month").value)])
-
-    Topaz_absence_graph = plt.bar(month, absences)
-
-
-    plt.show(Topaz_absence_graph)
-    plt.title("Topaz's Absenses Over a Month")
-    plt.xlabel('Month')
-    plt.ylabel('Absences')
+    plt.show(Academic_Attendance_Sheet )
+    plt.title("Academic Attendance sheet")
+    plt.xlabel("Month")
+    plt.ylabel("Absentees")
