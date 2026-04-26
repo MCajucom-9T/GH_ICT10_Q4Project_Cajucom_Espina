0c301ac5ba28eb1cbe50a2bd93990082bfbb766f
KEspina-10T <espina.ka@obmontessori.edu.ph>
2026-04-26 19:21:10 +0800

    Update main.py

--- gallery/main.py
+++ gallery/main.py
@@ -1,9 +1,9 @@
-
+#Displaying images in Gallery
 from pyscript import display, document
 
 
-def disp_images(e):
+def disp_images(e): #def to display image
     img = document.getElementById("gallery")
 
-    if img.style.display == "none":
+    if img.style.display == "none": 
         img.style.display = "flex"
