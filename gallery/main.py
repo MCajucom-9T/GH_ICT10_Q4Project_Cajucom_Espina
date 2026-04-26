114437bfd8fcbc23ed88d89d31cf890a95c3847d
KEspina-10T <espina.ka@obmontessori.edu.ph>
2026-04-26 19:39:19 +0800

    Update main.py

--- gallery/main.py
+++ gallery/main.py
@@ -3,7 +3,7 @@ from pyscript import display, document
 
 
 def disp_images(e): #def to display image
-    img = document.getElementById("gallery")
+    img = document.getElementById("gallery") #Gets the element with ID of Gallery
 
-    if img.style.display == "none": 
+    if img.style.display == "none": #Checks if gallery is hidden. If it is hidden, it will change the display to flex.
         img.style.display = "flex"
