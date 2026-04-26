b7d6ac8eb402055179238d87ed39aa19c3b54d3a
MCajucom-9T <cajucom.mnc@obmontessori.edu.ph>
2026-04-26 18:44:12 +0800

    Update main.py

--- gallery/main.py
+++ gallery/main.py
@@ -1,32 +1,9 @@
-# Object Oriented Programming
+
 from pyscript import display, document
 
-class Student: 
-    def __init__(self, name, section, sub):
-        self.name = name
-        self.section = section
-        self.sub = sub
-    
-classmates = [Student('Martina', 'Topaz', 'TLE'), 
-            Student('Miguel', 'Topaz', 'VE'), 
-            Student('Khloe', 'Topaz', 'SS'),
-            Student('AJ', 'Topaz', 'ICT'),
-            Student('Sim', 'Topaz', 'PE')]
 
-def class_info(e):
-    document.getElementById('display1').innerHTML = ''
+def disp_images(e):
+    img = document.getElementById("gallery")
 
-    name = document.getElementById("input1").value
-    section = document.getElementById("input2").value
-    sub = document.getElementById("input3").value
-
-    classmate = Student(name, section, sub)
-    classmates.append(classmate)
-
-    for classmate in classmates:
-        display(f'Hi, I am {classmate.name} from {classmate.section}. My favorite subjects is {classmate.sub}', target='display1')
-
-def introduce(e):
-    name = document.getElementById("input1").value
-
-    display(f'We have added {name} to the list.', target='display1')
+    if img.style.display == "none":
+        img.style.display = "flex"
